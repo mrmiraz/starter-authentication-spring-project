dfe7f167010a9216afe8ee115d6bc337442717d1
miraz <mahmudurrahmanmiraz143@gmail.com>
2025-07-01 00:24:54 +0600

    Sign out implementation

--- src/main/java/com/example/test/domain/dto/SignOutRequest.java
+++ src/main/java/com/example/test/domain/dto/SignOutRequest.java
@@ -5,6 +5,5 @@ import lombok.Data;
 @Data
 public class SignOutRequest {
     private String refreshToken;
-    private String accessToken;
-    private String clientInstanceId;;
+    private String clientInstanceId;
 }
